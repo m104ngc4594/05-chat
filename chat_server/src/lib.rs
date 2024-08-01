e22c2c3fea49c379de12675b9de929a34c9f7ed4
m104 <m104ngc4594@sina.com>
2024-08-01 08:57:32 +0800

    feature: support db migration and user model

--- chat_server/src/lib.rs
+++ chat_server/src/lib.rs
@@ -1,5 +1,7 @@
 mod config;
+mod error;
 mod handlers;
+mod models;
 
 use handlers::*;
 use std::{ops::Deref, sync::Arc};
@@ -10,6 +12,8 @@ use axum::{
 };
 
 pub use config::AppConfig;
+pub use error::AppError;
+pub use models::User;
 
 #[derive(Debug, Clone)]
 pub(crate) struct AppState {
--- migrations/20240726092616_initial.sql
+++ migrations/20240726092616_initial.sql
@@ -1,19 +1,25 @@
--- this file is used for postgresql database initialization
+-- Add migration script here
+
 -- create user table
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
     fullname VARCHAR(64) NOT NULL,
     email VARCHAR(64) NOT NULL,
     -- hashed argon2 password
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 -- create index for users for email
 CREATE UNIQUE INDEX IF NOT EXISTS email_index ON users(email);
 
 -- create chat type: single, group, private_channel, public_channel
-CREATE TYPE chat_type AS ENUM ('single', 'group', 'private_channel', 'public_channel');
+CREATE TYPE chat_type AS ENUM (
+    'single',
+    'group',
+    'private_channel',
+    'public_channel'
+);
 
 -- create chat table
 CREATE TABLE IF NOT EXISTS chats (
@@ -22,24 +28,21 @@ CREATE TABLE IF NOT EXISTS chats (
     type chat_type NOT NULL,
     -- user id list
     numbers BIGINT[] NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 -- create message table
 CREATE TABLE IF NOT EXISTS messages (
     id BIGSERIAL PRIMARY KEY,
-    chat_id BIGINT NOT NULL,
-    sender_id BIGINT NOT NULL,
+    chat_id BIGINT NOT NULL REFERENCES chats(id),
+    sender_id BIGINT NOT NULL REFERENCES users(id),
     content TEXT NOT NULL,
     images TEXT[],
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-
-    FOREIGN KEY (chat_id) REFERENCES chats(id),
-    FOREIGN KEY (sender_id) REFERENCES users(id)
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 -- create index for messages for chat_id and created_at order by created_at desc
 CREATE INDEX IF NOT EXISTS chat_id_index ON messages(chat_id, created_at DESC);
 
 -- create index for messages for sender_id
-CREATE INDEX IF NOT EXISTS sender_id_index ON messages(sender_id);
+CREATE INDEX IF NOT EXISTS sender_id_index ON messages(sender_id, created_at DESC);
