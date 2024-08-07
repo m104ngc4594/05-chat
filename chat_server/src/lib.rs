1732e8cb8349714cd6e3e521dd5a5365d8b71bfb
m104 <m104ngc4594@sina.com>
2024-08-07 16:15:34 +0800

    feature: support jwt for signin & signup api

--- chat_server/src/lib.rs
+++ chat_server/src/lib.rs
@@ -2,9 +2,13 @@ mod config;
 mod error;
 mod handlers;
 mod models;
+mod utils;
 
+use anyhow::Context;
 use handlers::*;
-use std::{ops::Deref, sync::Arc};
+use sqlx::PgPool;
+use std::{fmt, ops::Deref, sync::Arc};
+use utils::{DecodingKey, EncodingKey};
 
 use axum::{
     routing::{get, patch, post},
@@ -12,7 +16,7 @@ use axum::{
 };
 
 pub use config::AppConfig;
-pub use error::AppError;
+pub use error::{AppError, ErrorOutput};
 pub use models::User;
 
 #[derive(Debug, Clone)]
@@ -21,13 +25,15 @@ pub(crate) struct AppState {
 }
 
 #[allow(unused)]
-#[derive(Debug)]
 pub(crate) struct AppStateInner {
     pub(crate) config: AppConfig,
+    pub(crate) dk: DecodingKey,
+    pub(crate) ek: EncodingKey,
+    pub(crate) pool: PgPool,
 }
 
-pub fn get_router(config: AppConfig) -> Router {
-    let state = AppState::new(config);
+pub async fn get_router(config: AppConfig) -> Result<Router, AppError> {
+    let state = AppState::try_new(config).await?;
 
     let api = Router::new()
         .route("/signin", post(signin_handler))
@@ -41,10 +47,12 @@ pub fn get_router(config: AppConfig) -> Router {
         )
         .route("/chat/:id/messages", get(list_message_handler));
 
-    Router::new()
+    let app = Router::new()
         .route("/", get(index_handler))
         .nest("/api", api)
-        .with_state(state)
+        .with_state(state);
+
+    Ok(app)
 }
 
 // 当我调用 state.config => state.inner.config
@@ -57,9 +65,53 @@ impl Deref for AppState {
 }
 
 impl AppState {
-    pub fn new(config: AppConfig) -> Self {
-        Self {
-            inner: Arc::new(AppStateInner { config }),
-        }
+    pub async fn try_new(config: AppConfig) -> Result<Self, AppError> {
+        let dk = DecodingKey::load(&config.auth.pk).context("load pk failed")?;
+        let ek = EncodingKey::load(&config.auth.sk).context("load sk failed")?;
+        let pool = PgPool::connect(&config.server.db_url)
+            .await
+            .context("connect to db failed")?;
+        Ok(Self {
+            inner: Arc::new(AppStateInner {
+                config,
+                dk,
+                ek,
+                pool,
+            }),
+        })
+    }
+}
+
+impl fmt::Debug for AppStateInner {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("AppStateInner")
+            .field("config", &self.config)
+            .finish()
+    }
+}
+
+#[cfg(test)]
+impl AppState {
+    pub async fn new_for_test(
+        config: AppConfig,
+    ) -> Result<(sqlx_db_tester::TestPg, Self), AppError> {
+        use sqlx_db_tester::TestPg;
+        let dk = DecodingKey::load(&config.auth.pk).context("load pk failed")?;
+        let ek = EncodingKey::load(&config.auth.sk).context("load sk failed")?;
+        let server_url = "postgres://postgres:postgres@localhost:5432"; //config.server.db_url.split('/').next().unwrap();
+        let tdb = TestPg::new(
+            server_url.to_string(),
+            std::path::Path::new("../migrations"),
+        );
+        let pool = tdb.get_pool().await;
+        let state = Self {
+            inner: Arc::new(AppStateInner {
+                config,
+                dk,
+                ek,
+                pool,
+            }),
+        };
+        Ok((tdb, state))
     }
 }
