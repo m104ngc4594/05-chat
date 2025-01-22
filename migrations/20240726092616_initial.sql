bba27759295a7ecc17256e64accc043e3b144dd7
m104 <zhoupeng16@sina.cn>
2025-01-22 10:55:39 +0800

    fix: limit should have default and all data should use camelCase

--- migrations/20240726092616_initial.sql
+++ migrations/20240726092616_initial.sql
@@ -50,7 +50,8 @@ CREATE TABLE IF NOT EXISTS chats (
     type chat_type NOT NULL,
     -- user id list
     members BIGINT[] NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (ws_id, name, members)
 );
 
 -- create message table
@@ -68,3 +69,6 @@ CREATE INDEX IF NOT EXISTS chat_id_index ON messages(chat_id, created_at DESC);
 
 -- create index for messages for sender_id
 CREATE INDEX IF NOT EXISTS sender_id_index ON messages(sender_id, created_at DESC);
+
+-- create index for chat members
+CREATE INDEX IF NOT EXISTS chat_members_index ON chats USING GIN(members);
